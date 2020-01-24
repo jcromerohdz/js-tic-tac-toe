e55773b43ab8742fe5afba513a39394f7affeaa5
jcromerohdz <jcromerohdz@gmail.com>
2020-01-24 11:43:52 -0800

    Performing Player function to add name and symbol

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const GameBoard = (() => {
-    let board = new new Array(9).fill(null);
+const gameBoard = (() => {
+    let board = new Array(9).fill(null);
     let getBoard = () => board;
 
     return {getBoard};
 })();
+
+const Player = (name, symbol) => {
+
+  let getName = () => name;
+  let getSymbol = () => symbol;
+
+  return {getName, getSymbol};
+};
+
+
+
+
+const gameFlow = (() => {
+
+})();
+
+
+player_1 = Player('Christian', 'X');
+player_2 = Player('Luis', 'O');
+
+console.log(player_1.getName(), player_1.getSymbol());
+console.log(player_2.getName(), player_2.getSymbol());
