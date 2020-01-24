5e2750cff3d9f58928f55e7637871fcb0231ded8
Luis Preza <preza.luis@gmail.com>
2020-01-24 14:40:34 -0600

    set positions, just X for now

--- app.js
+++ app.js
@@ -1,10 +1,3 @@
-const gameBoard = (() => {
-    let board = new Array(9).fill(null);
-    let getBoard = () => board;
-
-    return {getBoard};
-})();
-
 const Player = (name, symbol) => {
 
   let getName = () => name;
@@ -13,16 +6,60 @@ const Player = (name, symbol) => {
   return {getName, getSymbol};
 };
 
+const domBoard = (()=>{
+  let dom = document.querySelectorAll('.boxcontainer div');
+  let forAll = (callback) => {
+    dom.forEach((item) => {
+      callback(item)
+    });
+    return dom;
+  }
 
+  let setPosition = (position, content) => {
+    dom[position].innerHTML = content;
+  }
 
+  return {forAll, setPosition}
+})();
 
-const gameFlow = (() => {
+const gameBoard = (() => {
+    let board = new Array(9).fill(null);
+    let getBoard = () => board;
+    let clear = ()  => domBoard.forAll(x=> x.innerHTML = '');
 
+    let setMove = (player, position) => {
+      if (!board[position]) {
+        board[position] = player.getSymbol();
+        domBoard.setPosition(position, player.getSymbol());
+      }
+    }
+
+    return {getBoard, clear, setMove};
 })();
 
 
-player_1 = Player('Christian', 'X');
-player_2 = Player('Luis', 'O');
+const gameFlow = (() => {
+  let player_1;
+  let player_2;
 
-console.log(player_1.getName(), player_1.getSymbol());
-console.log(player_2.getName(), player_2.getSymbol());
+  let startGame = () => {
+    player_1 = Player('Christian', 'X');
+    player_2 = Player('Luis', 'O');
+    return true;
+  }
+
+  let move = () => {
+      gameBoard.setMove(player_1, event.target.dataset.id);
+  }
+
+  return {move, startGame};
+
+})();
+//document.querySelector('.button.cancel').addEventListener('click', closeModal);
+domBoard.forAll(x => {
+  x.addEventListener('click', gameFlow.move);
+});
+
+
+gameBoard.clear();
+gameFlow.startGame();
